9ac3427ec6abccf26c8106fd8a3a6485b49f5e3a
white1984j <white1984j@gmail.com>
2018-05-04 00:10:16 +0300

    add preview-news

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,22 @@
 $(function() {
 
-	// Custom JS
+	
+  //news slider
+  $('.news-preview-slider').slick({
+    slidesToShow: 3,
+    prevArrow: '<button class="slick-arrow slick-arrow--prev"><i class="icon icon-arrow-blue"></i></button>',
+    nextArrow: '<button class="slick-arrow slick-arrow--next"><i class="icon icon-arrow-blue"></i></button>',
+    responsive: [
+      {
+        breakpoint: 991,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+          dots: true
+        }
+      }
+    ]
+  })
+
 
 });
--- gulpfile.js
+++ gulpfile.js
@@ -40,6 +40,7 @@ gulp.task('styles', function() {
 gulp.task('js', function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/slick-carousel/slick/slick.min.js',
 		'app/js/common.js', // Always at the end
 		])
 	.pipe(concat('scripts.min.js'))
